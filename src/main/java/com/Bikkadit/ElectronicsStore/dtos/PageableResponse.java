d1dd8d1214e692096d71271c9f5c9f98c1ceeb68
Pallavi0593 <pallavi.dhatkar@gmail.com>
2023-06-06 07:12:05 +0530

    PageableResponse

---
+++ src/main/java/com/Bikkadit/ElectronicsStore/dtos/PageableResponse.java
@@ -0,0 +1,21 @@
+package com.Bikkadit.ElectronicsStore.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class PageableResponse<T> {
+    private List<T> content;
+    private Integer pageNumber;
+    private Integer pageSize;
+    private Long totalElements;
+    private Integer totalPages;
+    private boolean lastpage;
+}
