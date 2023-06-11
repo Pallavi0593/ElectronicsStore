83b418261be1374907afb9af3fbafd8fc7a42313
Pallavi0593 <pallavi.dhatkar@gmail.com>
2023-06-11 21:47:28 +0530

    VAlidation added In CategoryDto

--- src/main/java/com/Bikkadit/ElectronicsStore/dtos/CategoryDto.java
+++ src/main/java/com/Bikkadit/ElectronicsStore/dtos/CategoryDto.java
@@ -1,5 +1,27 @@
 package com.Bikkadit.ElectronicsStore.dtos;
 
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CategoryDto {
 
+    @NotNull
+    private String categoryId;
+
+    @NotNull
+    @Size(min=10,max=20,message ="Min size for category title is 10")
+    private  String  title;
+
+    @NotNull
+    @Size(min=10,max=20,message ="Min size for category title is 10")
+    private  String desciption;
+    @NotNull
+    private  String coverImage;
 }
