147c48d207c439c8b04d8a11e83a263878a10ba5
Pallavi0593 <pallavi.dhatkar@gmail.com>
2023-06-11 20:25:39 +0530

    Category Service Interface Created

--- src/main/java/com/Bikkadit/ElectronicsStore/entities/Category.java
+++ src/main/java/com/Bikkadit/ElectronicsStore/entities/Category.java
@@ -1,4 +1,20 @@
 package com.Bikkadit.ElectronicsStore.entities;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table (name="Category_table")
+@Entity
 public class Category {
+
+    private String categoryId;
+    private  String  title;
+    private  String desciption;
+    private  String coverImage;
 }
