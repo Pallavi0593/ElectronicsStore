f65b48cd6703481311d4ccbf20ebb2941e3c4d6a
Pallavi0593 <pallavi.dhatkar@gmail.com>
2023-06-11 20:47:45 +0530

    Category Repository Created

--- src/main/java/com/Bikkadit/ElectronicsStore/repositories/CategoryRepository.java
+++ src/main/java/com/Bikkadit/ElectronicsStore/repositories/CategoryRepository.java
@@ -1,5 +1,6 @@
 package com.Bikkadit.ElectronicsStore.repositories;
 
+import com.Bikkadit.ElectronicsStore.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category,String> {
