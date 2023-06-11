ca643d5e54908ebbdefcd70654489a3b7963a971
Pallavi0593 <pallavi.dhatkar@gmail.com>
2023-06-11 21:44:24 +0530

    Fields Added in category class

--- src/main/java/com/Bikkadit/ElectronicsStore/entities/Category.java
+++ src/main/java/com/Bikkadit/ElectronicsStore/entities/Category.java
@@ -2,6 +2,7 @@ package com.Bikkadit.ElectronicsStore.entities;
 
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -13,9 +14,15 @@ import javax.persistence.Table;
 @Table (name="Category_table")
 @Entity
 public class Category {
-@Id
-    private String categoryId;
-    private  String  title;
-    private  String desciption;
+      @Id
+      @Column(name="id")
+       private String categoryId;
+
+     @Column(name="Category_title",length=60,nullable = false)
+      private  String  title;
+
+      @Column(name="Category_Descri",length=60)
+      private  String desciption;
+
     private  String coverImage;
 }
