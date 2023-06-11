52e3d35a7bb4730e6bf54ab98e5fe08499a4d9a2
Pallavi0593 <pallavi.dhatkar@gmail.com>
2023-06-11 20:48:24 +0530

    Category Service interface Created

--- src/main/java/com/Bikkadit/ElectronicsStore/Services/CategoryService.java
+++ src/main/java/com/Bikkadit/ElectronicsStore/Services/CategoryService.java
@@ -1,4 +1,28 @@
 package com.Bikkadit.ElectronicsStore.Services;
 
+import com.Bikkadit.ElectronicsStore.dtos.PageableResponse;
+import com.Bikkadit.ElectronicsStore.dtos.UserDto;
+
+import java.util.List;
+
 public interface CategoryService {
+    UserDto createCategory(CategoryDto categoryDto);
+
+    //update
+    UserDto UpdateUser(CategoryDto categoryDto,String categoryId);
+    //delete
+    void deleteuser(String userId);
+
+
+
+    //getUserById
+    UserDto getUserById(String userId);
+    //getAllUser
+
+    PageableResponse<UserDto> getAllUser(Integer pageNumber, Integer pageSize, String sortBy, String sortDir);
+
+    UserDto getUserByEmail(String email);
+
+    public List<UserDto> SearchUser(String keyword);
+}
 }
