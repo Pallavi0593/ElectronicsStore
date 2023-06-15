e75cc32f98756c762491c24782394575ba1212e4
Pallavi0593 <pallavi.dhatkar@gmail.com>
2023-06-15 21:44:35 +0530

    ProductRepository created

--- src/main/java/com/Bikkadit/ElectronicsStore/repositories/ProductRepository.java
+++ src/main/java/com/Bikkadit/ElectronicsStore/repositories/ProductRepository.java
@@ -1,8 +1,17 @@
 package com.Bikkadit.ElectronicsStore.repositories;
 
+import com.Bikkadit.ElectronicsStore.dtos.PageableResponse;
 import com.Bikkadit.ElectronicsStore.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product,String> {
 
-}
+     PageableResponse<List<Product>> findByTitleContaining(String subtitle) ;
+
+    PageableResponse<List<Product>> findByLive();
+
+   }
+
+
