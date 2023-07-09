91c5be02601f4abfa8b0029fb911a03b8cfa5db2
Pallavi0593 <pallavi.dhatkar@gmail.com>
2023-07-09 17:47:07 +0530

    Product repo

--- src/main/java/com/Bikkadit/ElectronicsStore/repositories/ProductRepository.java
+++ src/main/java/com/Bikkadit/ElectronicsStore/repositories/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.Bikkadit.ElectronicsStore.repositories;
 
+import com.Bikkadit.ElectronicsStore.entities.Category;
 import com.Bikkadit.ElectronicsStore.entities.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +11,8 @@ public interface ProductRepository extends JpaRepository<Product,String> {
     Page<Product> findByTitleContaining(String subtitle,Pageable  pageable) ;
 
     Page<Product> findByLiveTrue(Pageable  pageable);
+
+    Page<Product> findByCategory(Category category,Pageable pageable);
 
    }
 
