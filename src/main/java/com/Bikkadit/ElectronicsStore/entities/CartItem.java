8b5ae528896109966026f44e92871f28e66ff56b
Pallavi0593 <123481869+Pallavi0593@users.noreply.github.com>
2023-07-10 21:01:40 +0530

    CartItem Module Modified

--- src/main/java/com/Bikkadit/ElectronicsStore/entities/CartItem.java
+++ src/main/java/com/Bikkadit/ElectronicsStore/entities/CartItem.java
@@ -9,9 +9,13 @@ public class CartItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer cartItemId;
     @OneToOne
+    @JoinColumn(name = "product_id")
     private  Product product;
 
     private Integer quantity;
 
     private Integer totalPricae;
+    @ManyToOne
+@JoinColumn(name = "cart_id")
+    private  Cart cart;
 }
