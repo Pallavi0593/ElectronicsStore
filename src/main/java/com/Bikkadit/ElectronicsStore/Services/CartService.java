a86c8464b80246f136cd76c5dd4c8eb392b4189b
Pallavi0593 <123481869+Pallavi0593@users.noreply.github.com>
2023-07-12 20:22:44 +0530

    CartService.java modified

--- src/main/java/com/Bikkadit/ElectronicsStore/Services/CartService.java
+++ src/main/java/com/Bikkadit/ElectronicsStore/Services/CartService.java
@@ -6,10 +6,17 @@ import com.Bikkadit.ElectronicsStore.dtos.CartDto;
 public interface CartService {
 
     //Add Item To cart
-    //Case !:
-    //CAse 2:
+    //Case !:Cart not available For particular User.We will Create Cart then Add item
+    //CAse 2:Cart Available: Add Item To Cart
 
      CartDto addItemToCart(String userId, AddItemToCartRequest request);
+
+     //Remove Item From Cart
+
+     void removeItemFromCart(String userId,Integer cartItems);
+
+     //Remove All Items From Cart
+     void clearCart(String userId);
 
 
 }
