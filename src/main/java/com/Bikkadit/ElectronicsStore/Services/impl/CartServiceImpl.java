30e76962e1ea40e7d58f25e838b9762fa60c825e
Pallavi0593 <123481869+Pallavi0593@users.noreply.github.com>
2023-07-12 20:23:17 +0530

    CartServiceImpl.java Modified

--- src/main/java/com/Bikkadit/ElectronicsStore/Services/impl/CartServiceImpl.java
+++ src/main/java/com/Bikkadit/ElectronicsStore/Services/impl/CartServiceImpl.java
@@ -1,4 +1,22 @@
 package com.Bikkadit.ElectronicsStore.Services.impl;
 
-public class CartServiceImpl {
+import com.Bikkadit.ElectronicsStore.Services.CartService;
+import com.Bikkadit.ElectronicsStore.dtos.AddItemToCartRequest;
+import com.Bikkadit.ElectronicsStore.dtos.CartDto;
+
+public class CartServiceImpl implements CartService {
+    @Override
+    public CartDto addItemToCart(String userId, AddItemToCartRequest request) {
+        return null;
+    }
+
+    @Override
+    public void removeItemFromCart(String userId, Integer cartItems) {
+
+    }
+
+    @Override
+    public void clearCart(String userId) {
+
+    }
 }
