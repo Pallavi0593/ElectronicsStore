8fe9f93ce27959a5cfe6cef4af02afad1e63ec14
Pallavi0593 <123481869+Pallavi0593@users.noreply.github.com>
2023-07-14 15:16:53 +0530

    ProductDto

--- src/main/java/com/Bikkadit/ElectronicsStore/dtos/ProductDto.java
+++ src/main/java/com/Bikkadit/ElectronicsStore/dtos/ProductDto.java
@@ -11,7 +11,7 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class ProductDto {
+public class ProductDto extends CustomFieldDto{
 
 
     private String productId;
