d3e02b7e40de23827c6ede3b7ac1600edd15c830
Pallavi0593 <123481869+Pallavi0593@users.noreply.github.com>
2023-07-14 20:10:00 +0530

    CategoryServiceTest

--- src/test/java/com/Bikkadit/ElectronicsStore/Services/CategoryServiceTest.java
+++ src/test/java/com/Bikkadit/ElectronicsStore/Services/CategoryServiceTest.java
@@ -1,13 +1,24 @@
 package com.Bikkadit.ElectronicsStore.Services;
 
+import com.Bikkadit.ElectronicsStore.repositories.CategoryRepository;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
 public class CategoryServiceTest {
+    @MockBean
+    private CategoryRepository categoryRepository;
 
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private ModelMapper mapper;
 
 
 }
