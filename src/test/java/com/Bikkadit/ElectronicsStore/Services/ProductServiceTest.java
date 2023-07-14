28eebb297cb1a66f1fbd1a951155112511d16459
Pallavi0593 <123481869+Pallavi0593@users.noreply.github.com>
2023-07-14 20:09:27 +0530

    ProductServiceTest

--- src/test/java/com/Bikkadit/ElectronicsStore/Services/ProductServiceTest.java
+++ src/test/java/com/Bikkadit/ElectronicsStore/Services/ProductServiceTest.java
@@ -1,10 +1,22 @@
 package com.Bikkadit.ElectronicsStore.Services;
 
+import com.Bikkadit.ElectronicsStore.repositories.ProductRepository;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
 public class ProductServiceTest {
+    @MockBean
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private ModelMapper mapper;
 }
